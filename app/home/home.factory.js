c09a65e5c111439726a7bdbbc25a50dbfb3c9416
Manasa <Manasad530@gmail.com>
2017-03-30 19:14:05 -0400

    code formatted

--- app/home/home.factory.js
+++ app/home/home.factory.js
@@ -1,24 +1,19 @@
 'use strict';
 
-angular.module('travelApp.factory', [] )
-.factory('homeFactory', ['$http', '$q',
-    function ($http, $q) {
-        var url = "/app/home/restaurants.json";
-
-        function getRestaurants(userSearch) {
-            var deferred = $q.defer();
- 
-            $http.get(url)
-            .then(function(response) {
-
-                deferred.resolve(response);
-            });
-
-            return deferred.promise;
+angular.module('travelApp.factory', [])
+    .factory('homeFactory', ['$http', '$q',
+        function ($http, $q) {
+            var url = "/app/home/restaurants.json";
+            function getRestaurants(userSearch) {
+                var deferred = $q.defer();
+                $http.get(url)
+                    .then(function (response) {
+                        deferred.resolve(response);
+                    });
+                return deferred.promise;
+            }
+            return {
+                getRestaurants: getRestaurants
+            };
         }
-
-        return {
-            getRestaurants: getRestaurants
-        };
-    }
-]);
+    ]);
--- app/home/home.js
+++ app/home/home.js
@@ -1,23 +1,19 @@
 'use strict';
 
 angular.module('travelApp.home', ['ngRoute', 'travelApp.home2'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
-  });
-}])
-
-.controller('HomeCtrl', ['$scope', '$http', 'homeService', function($scope,$http, homeService) {
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'home/home.html',
+      controller: 'HomeCtrl'
+    });
+  }])
+  .controller('HomeCtrl', ['$scope', '$http', 'homeService', function ($scope, $http, homeService) {
     $scope.message = '';
-     $scope.name= name.name;
+    $scope.name = name.name;
 
     homeService.getRestaurants().then(function (data) {
-          $scope.names = data.data;
-          $scope.order = data.data.value;
-            console.log("oders:"+$scope.names);
-
+      $scope.names = data.data;
+      $scope.order = data.data.value;
+      console.log("oders:" + $scope.names);
     });
-  
-}]);
+  }]);
